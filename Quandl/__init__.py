72a701c19867735e9a1c6082ec201fe028fcf8dd
Alex Kestner <alex.kestner@gmail.com>
2014-02-28 15:57:39 -0500

    got rid of weird circular-ish import in setup.py, added requirements.txt and bumped .python-version to 2.7.6

--- Quandl/__init__.py
+++ Quandl/__init__.py
@@ -1,14 +1,2 @@
 # -*- coding: utf-8 -*-
-
-__authors__ = (
-    'Mark Hartney',
-    'Chris Stevens',
-    'Alex Kestner'
-)
-__maintainer__ = 'Alex Kestner'
-__email__ = 'alex.kestner@gmail.com'
-__url__ = 'http://www.quandl.com/'
-__license__ = 'MIT'
-__version__ = '1.5'
-
-from .Quandl import get, push, search
+from . import Quandl
--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+from pip.req import parse_requirements
 
-import Quandl
-
-setup(name = 'Quandl',
-      description = 'Package for Quandl API access',
-      version = Quandl.__version__,
-      author = ", ".join(Quandl.__authors__),
-      maintainer = Quandl.__maintainer__,
-      maintainer_email = Quandl.__email__,
-      url = Quandl.__url__,
-      license = Quandl.__license__,
-      install_requires = ["pandas", "numpy"],
-      packages = ['Quandl'],
+setup(
+    name = 'Quandl',
+    description='Package for Quandl API access',
+    version='1.5.1',
+    author=", ".join(['Mark Hartney','Chris Stevens','Alex Kestner']),
+    maintainer='Alex Kestner',
+    maintainer_email='alex.kestner@gmail.com',
+    url='http://www.quandl.com/',
+    license='MIT',
+    install_requires=[str(ir.req) for ir in parse_requirements('requirements.txt')],
+    packages=['Quandl'],
 )
